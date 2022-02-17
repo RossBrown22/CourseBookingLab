1aac85241dfeb18aa101fab4a75222913298ae08
Ross Brown <rossbrown22@hotmail.co.uk>
2022-02-17 14:49:03 +0000

    TDD implemented

--- src/test/java/com/example/coursebooking/CoursebookingApplicationTests.java
+++ src/test/java/com/example/coursebooking/CoursebookingApplicationTests.java
@@ -1,13 +1,35 @@
 package com.example.coursebooking;
 
+import com.example.coursebooking.repositories.BookingRepository;
+import com.example.coursebooking.repositories.CourseRepository;
+import com.example.coursebooking.repositories.CustomerRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class CoursebookingApplicationTests {
 
+	@Autowired
+	CustomerRepository customerRepository;
+	@Autowired
+	BookingRepository bookingRepository;
+	@Autowired
+	CourseRepository courseRepository;
+
 	@Test
 	void contextLoads() {
 	}
-	
+
+	@Test
+	public void canFindCourseByRating(){
+		assertEquals(1, courseRepository.findByRating(4).size());
+	}
+
+	@Test
+	public void canFindBookingByDate(){
+		assertEquals(2, bookingRepository.findByDate("07-11-21").size());
+	}
 }
